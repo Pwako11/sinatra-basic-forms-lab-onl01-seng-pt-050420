f9edfdbff2685c54ef934cac545c6342a2376e03
Patrick A <akolo@hotmail.com>
2020-08-02 22:49:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,9 +8,8 @@ class App < Sinatra::Base
     erb :index
   end
 
-
   post '/create_puppy' do
-
+     @new_pooch = Puppy.new(params[:name], params[:breed], params[:age])
     erb :create_puppy
   end
 
