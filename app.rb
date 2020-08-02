8f1b47a26fc9142e5b7ff1ebfb8ea684199b2255
Patrick A <akolo@hotmail.com>
2020-08-02 22:24:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,4 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/index' do
+
+    erb :index 
+  end
+
+
+  post '/create_puppy' do
+
+    erb :create_puppy
+  end
+
+  get '/display_puppy' do 
+
+    erb :display_puppy
+  end 
+
 end
